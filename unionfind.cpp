8084b3b475b25a54037cd9b2f0044b834dcb35de
Frestho <robyang17@gmail.com>
2020-12-21 20:54:21 -0800

    test

---
+++ unionfind.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 1e5 + 69;
+
+using ll = long long;
+
+using vi = vector<int>;
+#define pb push_back
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+
+using pii = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+void setIO(string name = "") { // name is nonempty for USACO file I/O
+    ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
+    // alternatively, cin.tie(0)->sync_with_stdio(0);
+    if (sz(name)) {
+        freopen((name+".in").c_str(), "r", stdin); // see Input & Output
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+struct DSU {
+    vi e; void init(int N) { e = vi(N,-1); }
+    // get representive component, uses path compression
+    int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+    bool sameSet(int a, int b) { return get(a) == get(b); }
+    int size(int x) { return -e[get(x)]; }
+    bool unite(int x, int y) { // union by size
+        x = get(x), y = get(y); if (x == y) return 0;
+        if (e[x] > e[y]) swap(x,y);
+        e[x] += e[y]; e[y] = x; return 1;
+    }
+};
+
+int main() {
+    setIO();
+    int N, Q; cin >> N >> Q;
+    DSU dsu;
+    dsu.init(N);
+    rep(i, 0, Q) {
+        int t, u, v; cin >> t >> u >> v;
+        if(t) {
+            if(dsu.sameSet(u, v)) cout << 1 << "\n";
+            else cout << 0 << "\n";
+        } else {
+            dsu.unite(u, v);
+        }
+    }
+}
